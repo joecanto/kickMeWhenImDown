79dcbbf8e69a02e3d41b658ae9074f8b65880010
Joe Cantwell <joe.cantwell@wpengine.com>
2017-09-12 10:27:31 +0100

    adding structure for new service

--- testme.sh
+++ testme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+http_proxy=104.198.132.138:4140
 for i in {1..1000}; do
-	http_proxy=104.198.132.138:4140 curl -s http://hello
+	http_proxy=$http_proxy curl -s http://insult/insult
 done
