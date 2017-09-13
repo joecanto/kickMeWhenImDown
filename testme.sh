32fbe539ad050b53a78c35933e5bcc49d5d8028c
Joe Cantwell <joe.cantwell@wpengine.com>
2017-09-13 12:06:23 +0100

    adding timeout to service

--- testme.sh
+++ testme.sh
@@ -2,6 +2,12 @@
 
 http_proxy=104.198.132.138:4140
 for i in {1..1000}; do
+	if [ $((i%5)) -eq 0 ]
+	then 
+		http_proxy=$http_proxy curl -s http://insult-now\?setTimeout\=long
+	else
+                http_proxy=$http_proxy curl -s http://insult-now
+	fi
+	
 	http_proxy=$http_proxy curl -s http://insult-rude
-	http_proxy=$http_proxy curl -s http://insult-now
 done
