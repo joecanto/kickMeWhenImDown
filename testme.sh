e099ef6130ddc35b87f32532cd12e98ae019e006
Eoghan Ryan <eoghan.ryan@wpengine.com>
2017-09-13 10:48:01 +0100

    working now - DO NOT TOUCH!!

--- testme.sh
+++ testme.sh
@@ -2,5 +2,6 @@
 
 http_proxy=104.198.132.138:4140
 for i in {1..1000}; do
-	http_proxy=$http_proxy curl -s http://insult/insult
+	http_proxy=$http_proxy curl -s http://insult-rude
+	http_proxy=$http_proxy curl -s http://insult-now
 done
